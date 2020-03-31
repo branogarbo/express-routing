6ccee797496d87af04a898266198eff04ae2ddba
Brian Longmore <brianl.ext@gmail.com>
2020-03-30 21:37:37 -1000

    deleted ejs

--- index.js
+++ index.js
@@ -3,16 +3,10 @@ const app = express();
 const port = 3000;
 
 app.listen(port, console.log(`listening on port ${port}`));
-app.set('view engine','ejs');
 
 app.get('/:user/search/:item?', (req,res)=>{
-  let params = req.params;
+  let p = req.params;
 
-  let ejsparams = {
-    title: 'testing out ejs',
-    user: params.user,
-    item: params.item
-  };
-
-  params.item ? res.render('pages/index', ejsparams) : res.send('please enter item to search for');
+  p.item ? res.send(`${p.user} is looking for ${p.item}`) : 
+    res.send('please enter item to search for');
 });
